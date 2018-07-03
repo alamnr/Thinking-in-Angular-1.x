bbe97b08587d404704287f8a5dbdadfb9902e0b2
alamnr <alamnr@gmail.com>
2018-07-03 18:12:57 +0600

    commit for controller as  syntax

--- nested-controllers/app.js
+++ nested-controllers/app.js
@@ -1,9 +1,22 @@
 var app = angular.module('nestedController',[]); 
 
+/*
 app.controller('Ctrl1',($scope)=>{
     $scope.testProp = 'From  controller 1';
 });
 
 app.controller('Ctrl2',($scope)=>{
    // $scope.testprop = 'From controller 2';
+});   */
+
+
+
+app.controller('Ctrl1',()=>{
+    
+    this.testProp  = 'From controller 1';
+});
+
+app.controller('Ctrl2',()=>{
+   
+   this.testProp  = 'From controller 2';
 });
