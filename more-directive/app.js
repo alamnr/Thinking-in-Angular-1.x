439a9379fd7af5e4479e9e34893eb1a26b93a9d8
alamnr <alamnr@gmail.com>
2018-07-04 10:07:54 +0600

    Commit for ngRepeat and scope in ng-Repeat

--- more-directive/app.js
+++ more-directive/app.js
@@ -1,3 +1,10 @@
 angular.module('moreDirectiveApp',[]).controller('moreDirectivesCtrl',function(){
+    this.myList = [1,2,3,4];
 
+    this.userList = [
+        {name:'foo', age:20},
+        {name:'bar', age:34},
+        {name:'joe', age:56},
+        {name:'doe', age:34},
+    ]
 });
